b20ead2483f3ec58a3274b91a4a6d8f67963c92f
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2024-01-03 22:25:39 +0330

    create get_path func to be imported and used in other modules. make changes to some path names

--- data_manager.py
+++ data_manager.py
@@ -1,7 +1,14 @@
 import os
 
-Raw_Data = 'Raw_Data'
-data_file_name = 'Daily_Car_Price_Telegram.txt'
-draft_file_name = 'draft.txt'
-data_path = os.path.join(Raw_Data, data_file_name)
-draft_path = os.path.join(Raw_Data, draft_file_name)
+RAW_DATA = 'Raw_Data'
+DATA_FILE_BASE_NAME = 'Daily_Car_Price_Telegram.txt'
+
+
+def get_path(base_dir=RAW_DATA, data_file_base_name=DATA_FILE_BASE_NAME, data_file_name_ext=""):
+    return os.path.join(base_dir, data_file_base_name+data_file_name_ext)
+
+
+
+example_data_file_name = 'draft.txt'
+data_path = get_path(base_dir=RAW_DATA, data_file_base_name=DATA_FILE_BASE_NAME)
+example_data_path = get_path(base_dir=RAW_DATA, data_file_base_name=example_data_file_name)
