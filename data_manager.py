55f217bf2808fbddb2704fee4c2926070d99f267
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2024-01-03 22:00:46 +0330

    create data_manager.py to separate managing data to a single file

---
+++ data_manager.py
@@ -0,0 +1,7 @@
+import os
+
+Raw_Data = 'Raw_Data'
+data_file_name = 'Daily_Car_Price_Telegram.txt'
+draft_file_name = 'draft.txt'
+data_path = os.path.join(Raw_Data, data_file_name)
+draft_path = os.path.join(Raw_Data, draft_file_name)
