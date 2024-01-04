04300b79c8faf55244ca7fccd1e26ce2b559e052
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2024-01-04 19:00:17 +0330

    add a func:  get_raw_text_of_daily_car_price_info

--- scrape_Telegram_car_prices.py
+++ scrape_Telegram_car_prices.py
@@ -22,17 +22,32 @@ with open(data_path, "r") as file:
 # scrapping for draft_content from
 
 # Define the pattern for extracting Jalaali dates and all car info between to consecutive dates
-pattern = r'📅(.+?)\n([\s\S]*?)(?=\n📅|$)'
+date_in_text_pattern = r'📅(.+?)\n([\s\S]*?)(?=\n📅|$)'
 # This re working depends on the fact that each Jalali dates has a '📅' marker and is in separate line
 # and  car prices info are between two  to separate lines
 
 # Extract Jalaali dates and car prices info of each day from the text
-matches = re.findall(pattern, draft_contents)
+matches = re.findall(date_in_text_pattern, draft_contents)
+
+
+def get_raw_text_of_daily_car_price_info(date_sign='📅', pattern=None, content=data_contents):
+    """
+
+    :param content:
+    :param date_sign:
+    :param pattern:
+           default value is fr'{date_sign}(.+?)\n([\s\S]*?)(?=\n{date_sign}|$)'
+
+    """
+    if pattern is None:
+        pattern = fr'{date_sign}(.+?)\n([\s\S]*?)(?=\n{date_sign}|$)'
+
+    return re.findall(pattern, content)
+
 
 # Create a table
 table = PrettyTable()
 table.field_names = ['Jalaali Date', 'Car Type', 'Car Price']
-
 
 # Add rows to the table
 for date, car_info in matches:
@@ -45,12 +60,14 @@ for date, car_info in matches:
         car_price = car_price.strip().replace(',', '')
         table.add_row([date.strip(), car_type, car_price])
 
+
+
+
 # Print the table
 print(table)
 
 # wtf = '📅'
 
 lines_of_draft = draft_contents.split('\n')
-
 
 # Question: Why are table titles misplaced? because farsi text directions?
