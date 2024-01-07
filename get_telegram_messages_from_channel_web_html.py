8205e47be71611a1b9ecaba03b59c19bb8165ec6
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2024-01-08 01:54:14 +0330

    create get_telegram_messages_from_channel_web_html.py and write first batch of html raw data

---
+++ get_telegram_messages_from_channel_web_html.py
@@ -0,0 +1,18 @@
+import data_manager
+
+html_data_path_html = data_manager.get_path(base_dir=data_manager.KHODROO_ROOZ_HTML_DIR_PATH,
+                                            data_file_base_name=data_manager.HTML_DATA_FILE_BASE_NAME,
+                                            data_file_name_ext='1_to_60.html')
+
+html_data_path_txt = data_manager.get_path(base_dir=data_manager.KHODROO_ROOZ_HTML_DIR_PATH,
+                                           data_file_base_name=data_manager.HTML_DATA_FILE_BASE_NAME,
+                                           data_file_name_ext='1_to_60.txt')
+
+with open(html_data_path_txt, 'r') as file:
+    html_content = file.read()
+
+# Write html data to html file
+# html_content = "<html><body>...</body></html>"
+
+with open(html_data_path_html, "w") as file:
+    file.write(html_content)
