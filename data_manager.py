5c0ea69d14b03c5db5ec504d51954d0639f9a7a0
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2024-01-07 18:25:54 +0330

    add if __name__...

--- data_manager.py
+++ data_manager.py
@@ -8,7 +8,7 @@ def get_path(base_dir=RAW_DATA, data_file_base_name=DATA_FILE_BASE_NAME, data_fi
     return os.path.join(base_dir, data_file_base_name+data_file_name_ext)
 
 
-
-example_data_file_name = 'draft.txt'
-data_path = get_path(base_dir=RAW_DATA, data_file_base_name=DATA_FILE_BASE_NAME)
-example_data_path = get_path(base_dir=RAW_DATA, data_file_base_name=example_data_file_name)
+if __name__ == '__main__':
+    example_data_file_name = 'draft.txt'
+    data_path = get_path(base_dir=RAW_DATA, data_file_base_name=DATA_FILE_BASE_NAME)
+    example_data_path = get_path(base_dir=RAW_DATA, data_file_base_name=example_data_file_name)
