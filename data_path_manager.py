62f60717cbea855842472260e8c0ac1536f600dc
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2024-01-11 11:18:35 +0330

    edit data_path_manager.py  and add check_project_path() method

--- data_path_manager.py
+++ data_path_manager.py
@@ -1,6 +1,8 @@
 import os
 
 PROJECT_ROOT_NAME = 'data_extraction'
+PROJECT_ROOT_PATH = '/home/jvn/GameTheorist/Computing/PythonProjects/data_extraction'
+
 DATA_BASE_DIR = 'Data'
 RAW_DATA_BASE_DIR_NAME = 'Raw_Data'
 PROCESSED_DATA_DIR_NAME = 'Processed_Data'
@@ -16,21 +18,35 @@ HTML_DATA_FILE_BASE_NAME = 'messages_container_'
 
 class PathConfig:
     def __init__(self, project_root_name=PROJECT_ROOT_NAME,
+                 project_root_path_abs=PROJECT_ROOT_PATH,
                  data_base_dir_name=DATA_BASE_DIR,
                  raw_data_base_dir_name=RAW_DATA_BASE_DIR_NAME,
                  processed_data_dir_name=PROCESSED_DATA_DIR_NAME,
-                 data_base_root='',
+                 data_base_root_path_rel='',
                  structure='STANDARD_TREE'):
         self.project_root_name = project_root_name
+        self.project_root_path_abs = project_root_path_abs
         self.data_base_dir_name = data_base_dir_name
         self.raw_data_base_dir_name = raw_data_base_dir_name
         self.processed_data_dir_name = processed_data_dir_name
         self.structure = structure
-        self.data_base_root = data_base_root
+        self.data_base_root_path_rel = data_base_root_path_rel
         if self.structure == 'STANDARD_TREE':
-            self.data_base_dir_path = os.path.join(self.data_base_root, self.data_base_dir_name)
-            self.raw_data_base_dir_path = os.path.join(self.data_base_dir_name, self.raw_data_base_dir_name)
-            self.processed_data_dir_path = os.path.join(self.data_base_dir_name, self.processed_data_dir_name)
+            self.data_base_dir_path_rel = os.path.join(self.data_base_root_path_rel, self.data_base_dir_name)
+            self.raw_data_base_dir_path_rel = os.path.join(self.data_base_dir_name, self.raw_data_base_dir_name)
+            self.processed_data_dir_path_rel = os.path.join(self.data_base_dir_name, self.processed_data_dir_name)
+
+    def check_project_path(self, print_warnings_regardless=False):
+        if (os.getcwd() != self.project_root_path_abs) or print_warnings_regardless:
+            print('Project Warning: project root path is different from current directory')
+            print(f'Parent of Current Working Directory Is {os.path.dirname(os.getcwd())}')
+            print(f'Project Root Path Is {self.project_root_path_abs}')
+        if (os.path.dirname(__file__) != self.project_root_path_abs) or print_warnings_regardless:
+            print('Project Warning: project root path is different from directory of current file')
+            print(f'Directory of Current file Is {os.path.dirname(__file__) }')
+            print(f'Project Root Path Is {self.project_root_path_abs}')
+        else:
+            print('Paths related to Project Root seems to be correct')
 
 
 DEFAULT_PATH_CONFIG = PathConfig()
