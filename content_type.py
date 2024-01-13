b66768e72c72c2a9efbd911873782d0e37c873d6
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2024-01-14 02:55:39 +0330

    start content_type.py and create a working version of ContentType class

---
+++ content_type.py
@@ -0,0 +1,35 @@
+class ContentType:
+    def __init__(self, name=None, base_file_system_rep=None, data_type=None, file_ext=None,
+                 naming_ext_convention_key='standard_from_start_to_end'):
+        self.name = name
+        if base_file_system_rep is None:
+            base_file_system_rep = self.name
+        self.base_file_system_rep = base_file_system_rep
+        self.data_type = data_type
+        if file_ext is None:
+            file_ext = self.get_file_ext_from_data_type()
+        self.file_ext = file_ext
+        self.naming_ext_convention_key = naming_ext_convention_key
+
+    def add_naming_ext(self, *args, base_fs_name=None, naming_convention_key=None, **kwargs):
+        if base_fs_name is None:
+            base_fs_name = self.base_file_system_rep
+        if naming_convention_key is None:
+            naming_convention_key = self.naming_ext_convention_key
+        print(args)
+        print(kwargs)
+        return NAMING_EXT_CONVENTIONS[naming_convention_key](base_fs_name, *args, **kwargs)
+
+    def get_file_ext_from_data_type(self, dtype=None):
+        if dtype is None:
+            dtype = self.data_type
+        return FILE_EXT_OF_DTYPES[dtype]
+
+
+FILE_EXT_OF_DTYPES = {'text': '.txt', 'html': '.html'}
+NAMING_EXT_CONVENTIONS = {'standard_from_start_to_end': lambda name, start_, end_: '_'.join([name, 'from', start_,
+                                                                                             'to', end_])
+                          }
+
+if __name__ == '__main__':
+    ct = ContentType(name='whole_text_of_all_messages', data_type='text', file_ext='.txt')
