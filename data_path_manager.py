edfec3c02781f6ac1fc84f243550d87d8182fca3
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2024-01-13 15:25:37 +0330

    delete get_path from data_path_manager.py and const path defined

--- data_path_manager.py
+++ data_path_manager.py
@@ -52,12 +52,5 @@ class PathConfig:
 DEFAULT_PATH_CONFIG = PathConfig()
 
 
-def get_path(base_dir=RAW_DATA_BASE_DIR_NAME, data_dir_relative_to_base_dir='',
-             data_file_base_name=DATA_FILE_BASE_NAME, data_file_name_ext=".txt"):
-    return os.path.join(base_dir, data_dir_relative_to_base_dir, data_file_base_name + data_file_name_ext)
-
-
 if __name__ == '__main__':
-    example_data_file_name = 'draft.txt'
-    data_path = get_path(base_dir=RAW_DATA_BASE_DIR_NAME, data_file_base_name=DATA_FILE_BASE_NAME)
-    example_data_path = get_path(base_dir=RAW_DATA_BASE_DIR_NAME, data_file_base_name=example_data_file_name)
+    pass
