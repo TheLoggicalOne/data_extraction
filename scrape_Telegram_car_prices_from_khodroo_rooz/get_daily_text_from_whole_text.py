05a45bcb61ee50d4497d5f6cb7a94c4a3286b5f5
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2024-01-14 02:54:20 +0330

    start get_daily_text_from_whole_text.py functions

--- scrape_Telegram_car_prices_from_khodroo_rooz/get_daily_text_from_whole_text.py
+++ scrape_Telegram_car_prices_from_khodroo_rooz/get_daily_text_from_whole_text.py
@@ -1,13 +1,21 @@
 import re
 import pandas as pd
 
-
 import data_path_manager
+import data_source
 
+def separate_whole_raw_text_to_daily_raw_text(date_sign='📅', pattern=None, content=data_contents):
+    """
+    :param content:
+    :param date_sign:
+    :param pattern:
+           default value is fr'{date_sign}(.+?)\n([\s\S]*?)(?=\n{date_sign}|$)'
 
+    """
+    if pattern is None:
+        pattern = fr'{date_sign}(.+?)\n([\s\S]*?)(?=\n{date_sign}|$)'
 
-
-
+    return re.findall(pattern, content)
 
 
 
