b5a06b5386dde7d4cd0f4865ea82f32831116e92
TheLoggicalOne <TheLoggicalOne@Gmail.Com>
2024-01-14 02:57:47 +0330

    start DataSource_info.py for scrape_Telegram_car_prices_from_khodroo_rooz  and create a contentType for whole text of this channel

---
+++ scrape_Telegram_car_prices_from_khodroo_rooz/DataSource_info.py
@@ -0,0 +1,13 @@
+from data_source import DataSource
+from content_type import ContentType
+Telegram_Daily_Car_Prices_DS = DataSource(name='khodroo_rooz',
+                                          source_type='Telegram_Channel',
+                                          source_id='@khodroo_rooz',
+                                          source_desc="""Telegram Channel with almost always one message per day 
+                                          containing daily new car prices gathered from car dealers and sellers""",
+                                          url='t.me/khodroo_rooz')
+DS = Telegram_Daily_Car_Prices_DS
+
+ct = ContentType(name='whole_text_of_all_messages', data_type='text',
+                 # file_ext='.txt'
+                 )
